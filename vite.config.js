3a055afbc3522dd0f6b387aec99241bca210c6f9
keerthivasan.b1-v <keerthivasan.b1-v@abfrl.adityabirla.com>
2026-04-16 23:22:08 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     base: `/${env.VITE_REPO_NAME}/`,
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), netlify()],
     server: {
       open: true,
       host: false, // Allow access from network devices
