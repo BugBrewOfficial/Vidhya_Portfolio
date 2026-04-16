5035219867b145c374e08e459095fa59aa11fcd8
cavin <cavin.balasubramaniam@vgmsofttech.com>
2026-04-16 18:43:26 +0530

    first commit

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,65 @@
+import person from "../../assets/person1.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import SocialMedia from "../common/socialMedia/SocialMedia";
+
+const Profile = () => {
+  return (
+    <div
+      className={`relative mx-4 xxl:mx-0.5 -bottom-20 lg:-bottom-28 z-10 rounded-2xl bg-white drop-shadow-2xl max-xl:mb-5 shadow-white xl:p-28 lg:p-20 md:p-16 sm:p-10 p-4`}
+      id="profile"
+    >
+      <div className="flex max-md:flex-col justify-between items-center gap-6">
+        {/* Profile image */}
+        <div className="xxl:max-w-106 w-auto h-auto xxl:max-h-126">
+          <div className="max-w-106 h-117 object-fill overflow-hidden rounded-xl">
+            <img
+              className="bg-soft-white h-[120%] object-cover"
+              src={person}
+              alt=""
+            />
+          </div>
+          {/* Social media section */}
+          <div className="relative bottom-9">
+            <div className="flex justify-center">
+              <div className="px-6 max-w-66 py-3 z-50 text-center bg-white rounded-[4px] center shadow-2xl drop-shadow-2xl shadow-white">
+                <SocialMedia />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="max-sm:w-full w-[33rem]">
+          <h2
+            className={`text-2xl xxs:text-3xl sm:text-4xl lg:text-[38px] text-[min(24px,38px)] max-md:text-center font-semibold mb-8`}
+          >
+            Vidhya Sri
+          </h2>
+          <p className="text-lg xxs:text-xl sm:text-2xl lg:text-[24px] text-[min(16px,24px)] max-md:text-center font-semibold mb-8">Founder & Head Beekeeper 'FutureNature'</p>
+          <div
+            className={`text-xs xs:text-[16px] lg:text-lg font-normal max-md:text-center text-gray-600`}
+          >
+            <p className={``}>
+              Honey is the only food on the planet that never spoils. It is nature's way of preserving energy. My mission isn't just to harvest it, but to protect the tiny architects who build it.
+            </p>
+            <p className="mt-3">
+              At FutureNature, we stepped away from industrial farming. We embraced the wild. Every jar you hold is a result of ethical patience harvested only when the bees have stored enough for themselves.
+            </p>
+          </div>
+          <div className="mt-8 flex max-md:justify-center">
+            <a
+              className="btn xxs:btn-lg px-6 max-xs:px-2 xxs:py-3 btn-primary text-xs xxs:text-[14px] sm:text-[16px]"
+              href="https://www.futurenature.in"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visit our site
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
